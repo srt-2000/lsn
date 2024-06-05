ac44fd4875b16cdce6d8815fdbf18631c64fec09
srt-2000 <srt2000888@gmail.ru>
2024-06-05 20:21:39 +0300

    adding after rewriting files lesson

--- py_project1/lesson1.py
+++ py_project1/lesson1.py
@@ -1,5 +1,22 @@
+import os
 import os.path
+with open('lesson_w.txt', 'w') as rezFile:
+    for currentDir, dirs, files in os.walk('main'):
+        for tmpFile in files:
+            if tmpFile.endswith('.py'):
+                rezFile.write(currentDir)
+                rezFile.write('\n')
+                break
 
-print(os.getcwd())
-print(os.listdir(".idea"))
-print(os.path.exists("gnom.py"))
+bands = []
+
+with open("lesson_w.txt") as fin:
+    for line in fin:
+        bands.append(line.strip())
+
+bands.sort()
+
+with open("sorted_w.txt", "w") as ft:
+    for band in bands:
+        ft.write(band + "\n")
+
